3e7590d3331268f7bc8191a422c5c12a961230ee
nyroro <359779828@qq.com>
2025-02-07 16:34:19 +0800

    commit

---
+++ CF2062A.py
@@ -0,0 +1,4 @@
+﻿t = int(input())
+for ti in range(t):
+    s = input()
+    print(s.count('1'))
---
+++ CF2062B.py
@@ -0,0 +1,7 @@
+﻿t = int(input())
+for ti in range(t):
+    n = int(input())
+    s = input()
+    arr = [int(t) for t in s.split()]
+    ans = any(arr[i]<= 2*i or arr[i]<= 2*(n-i-1) for i in range(n))
+    print("YES" if not ans else "NO")
