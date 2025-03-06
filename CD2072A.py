3d4fd1c22058d10e5ad53e9ed3d744ac0d75fb74
nyroro <359779828@qq.com>
2025-03-06 18:14:01 +0800

    daily

---
+++ CD2072A.py
@@ -0,0 +1,10 @@
+﻿def solve():
+    n,k,p = map(int,input().split())
+    v = abs(k)//p + (1 if abs(k)%p!=0 else 0)
+    if v <= n:
+        print(v)
+    else:
+        print(-1)
+
+for i in range(int(input())):
+    solve()
---
+++ CF2070D.py
@@ -0,0 +1,30 @@
+﻿mod = 998244353
+def solve():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    d = [0]*(n+1)
+    p = [0]*(n+1)
+    tot = [0]*n
+    tot[0] = 1
+    arr2 = []
+    for i,t in enumerate(arr):
+        j = i+2
+        d[j] = d[t]+1
+        p[j] = t
+        arr2.append((d[j], j))
+    arr2.sort()
+    dp = [0]*(n+1)
+    dp[1] = 1
+    for d, x in arr2:
+        # print(d,x)
+        if d == 1:
+            dp[x] = 1
+        else:
+            dp[x] = (tot[d-1]-dp[p[x]]+mod)%mod
+        tot[d]+=dp[x]
+        tot[d]%=mod
+    # print(dp)
+    print(sum(dp)%mod)
+
+for i in range(int(input())):
+    solve()
