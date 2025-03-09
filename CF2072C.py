98f367f975607fc4b49938d93aec2a55ef8b84a8
nyroro <359779828@qq.com>
2025-03-09 22:56:19 +0800

    daily

---
+++ CF2072C.py
@@ -0,0 +1,23 @@
+﻿def solve():
+    n,x = map(int, input().split())
+
+    arr = [0]
+    tx = x
+    val = 0
+    while len(arr)<n and tx > 0:
+        v = tx & (-tx)
+        val += v
+        m = len(arr)
+        for i in range(m):
+            arr.append(v+arr[i])
+            if len(arr) >= n:
+                break
+        tx -= v
+    while len(arr)<n:
+        arr.append(x)
+    if val < x:
+        arr[-1] = x
+
+    print(' '.join([str(t) for t in arr]))
+for i in range(int(input())):
+    solve()
