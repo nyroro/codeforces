8dfa25b8d32e4d401868bb9a8c6e922214e73095
nyroro <359779828@qq.com>
2025-03-11 15:44:23 +0800

    daily

---
+++ CF2077A.py
@@ -0,0 +1,17 @@
+﻿def solve():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    ret = []
+    arr.sort()
+    a0 = arr[0]
+    ret.append(arr[2*n-1])
+    s = 0
+    for i in range(1, n):
+        ret.append(arr[i])
+        ret.append(arr[i+n-1])
+        s += arr[i+n-1] - arr[i]
+    ret.append(s+arr[2*n-1]+a0)
+    ret.append(a0)
+    print(' '.join([str(t) for t in ret]))
+for i in range(int(input())):
+    solve()
