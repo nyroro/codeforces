657917cb3ca0ff27adf7ea719f2d8767ccfa7d42
nyroro <359779828@qq.com>
2025-03-14 14:27:39 +0800

    daily

---
+++ CF2052J.py
@@ -0,0 +1,51 @@
+﻿import bisect
+def solve():
+    n,m,q = map(int,input().split())
+    arr = list(map(int, input().split()))
+    d = list(map(int, input().split()))
+    l = list(map(int, input().split()))
+    s = [l[0]]
+    for i in range(1,m):
+        s.append(l[i]+s[-1])
+    arr = sorted([(d, a) for a, d in zip(arr,d)])
+    que = []
+    index = 0
+    while index < len(arr):
+        ed, work_time = arr[index]
+        st = ed-work_time
+        if len(que) == 0:
+            # st, ed, watch_time
+            watch_time = st
+            que.append((st, ed, watch_time))
+            index += 1
+        elif st > que[-1][1]:
+            watch_time = st - que[-1][1]
+            que.append((st, ed, watch_time + que[-1][2]))
+            index += 1
+        else:
+            lst, led, lwatch_time = que[-1]
+            que.pop()
+            work_time += led-lst
+            arr[index] = (ed, work_time)
+    # print(que)
+    t = list(map(int, input().split()))
+    ret = []
+    for x in t:
+        i = bisect.bisect_left(que, (x, 0x7fffffff, 0))
+        if i == 0:
+            watch_time = x
+        else:
+            watch_time = que[i-1][2] + max(x - que[i-1][1], 0)
+        j = bisect.bisect_right(s, watch_time)
+        ret.append(j)
+        # print('For',x)
+        # print('Find',i)
+        # print('s',s)
+        # print('watch_time', watch_time)
+        # print('ret', j)
+        # print('===============')
+    print(' '.join(str(t) for t in ret))
+            
+    
+for i in range(int(input())):
+    solve()
---
+++ CF2074G.py
@@ -0,0 +1,14 @@
+﻿def solve():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    dp = [[0]*(n+1) for i in range(n+1)]
+    ret = 0
+    for l in range(n-1, -1, -1):
+        for r in range(l,n):
+            for k in range(l,r):
+                dp[l][r] = max(dp[l][r], dp[l][k]+dp[k+1][r])
+            for k in range(l+1,r):
+                dp[l][r] = max(dp[l][r], dp[l+1][k-1]+dp[k+1][r-1]+arr[l]*arr[k]*arr[r])
+    print(dp[0][n-1])
+for i in range(int(input())):
+    solve()
