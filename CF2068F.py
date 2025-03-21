e83bc7db25a3b1e387f7cee41a9be18f4f59e086
nyroro <359779828@qq.com>
2025-03-21 22:50:29 +0800

    daily

---
+++ CF2068F.py
@@ -0,0 +1,74 @@
+﻿n = int(input())
+s = []
+for i in range(n):
+    s.append(input())
+t = input()
+def issub(ss, t):
+    if len(ss)<len(t):
+        return False
+    i = 0
+    j = 0
+    while i<len(ss) and j<len(t):
+        if ss[i] == t[j]:
+            i += 1
+            j += 1
+        else:
+            i+=1
+    return j == len(t)
+
+for ss in s:
+    if issub(ss,t):
+        print('NO')
+        exit(0)
+
+index_t = 0
+ind = [0] * n
+cha = [[] for i in range(26)]
+que = []
+ans = []
+sat = 0
+qi = 0
+for i, ss in enumerate(s):
+    c = ord(ss[0]) - ord('a')
+    if len(cha[c]) == 0 and ss[0] != t[0]:
+        que.append(c)
+    cha[c].append(i)
+# print('que',que)
+while sat < n:
+    # print('sat', sat, n, ind)
+    if qi >= len(que):
+        c = ord(t[index_t]) - ord('a')
+        ans.append(t[index_t])
+        index_t += 1
+        # print('add index_t', index_t)
+        arr = cha[c]
+        cha[c] = []
+        for i in arr:
+            ind[i]+=1
+            if ind[i] == len(s[i]):
+                sat += 1
+            else:
+                si = s[i][ind[i]]
+                cc = ord(si) - ord('a')
+                if len(cha[cc]) == 0 and si != t[index_t]:
+                    que.append(cc)
+                cha[cc].append(i)
+    else:
+        c = que[qi]
+        # print('handle', c, ans)
+        qi += 1
+        ans.append(chr(ord('a')+c))
+        arr = cha[c]
+        cha[c] = []
+        for i in arr:
+            ind[i]+=1
+            if ind[i] == len(s[i]):
+                sat += 1
+            else:
+                si = s[i][ind[i]]
+                cc = ord(si) - ord('a')
+                if len(cha[cc]) == 0 and si != t[index_t]:
+                    que.append(cc)
+                cha[cc].append(i)
+print('YES')
+print(''.join(ans))
---
+++ CF2073A.py
@@ -0,0 +1,37 @@
+﻿n,m = map(int,input().split())
+
+mat = []
+row = [0]*n
+col = [0]*m
+for i in range(n):
+    mat.append(input())
+    for j in range(m):
+        if mat[i][j] == '.':
+            row[i]+=1
+            col[j]+=1
+
+ret = 0
+for i in range(n):
+    if row[i] >= 4:
+        v = row[i]
+        ret += v*(v-1)*(v-2)*(v-3)
+    s = 0
+    for j in range(m):
+        if mat[i][j] == '.':
+            ret += s * (col[j]-1)*2
+            if row[i] > 2:
+                ret += (col[j]-1)*(row[i]-1)*(row[i]-2)*2
+            s+=col[j] - 1
+row,col = col,row
+for i in range(m):
+    if row[i] >= 4:
+        v = row[i]
+        ret += v*(v-1)*(v-2)*(v-3)
+    s = 0
+    for j in range(n):
+        if mat[j][i] == '.':
+            ret += s * (col[j]-1)*2
+            if row[i] > 2:
+                ret += (col[j]-1)*(row[i]-1)*(row[i]-2)*2
+            s+=col[j] - 1
+print(ret)
