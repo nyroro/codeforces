4a24e5fb6cc24f99f40c7d32010d78f99210c5ad
nyroro <359779828@qq.com>
2025-04-09 14:28:17 +0800

    daily

---
+++ CF2051F.py
@@ -0,0 +1,35 @@
+﻿def solve():
+    n,m,k = map(int,input().split())
+    arr = list(map(int, input().split()))
+    mid = [m,m]
+    l = 0
+    r = n+1
+    for t in arr:
+        if l > 0 and t > l:
+            l += 1
+        if r <= n and t < r:
+            r -= 1
+        if t < mid[0]:
+            mid[0] = max(mid[0]-1, 1)
+        elif t > mid[1]:
+            mid[1] = min(mid[1]+1, n)
+        else:
+            l = max(l, 1)
+            r = min(r, n)
+            if mid[1] == t and mid[0] == t:
+                mid = [n,n]
+        if mid[0] <= l+1:
+            mid[0] = 1
+        if mid[1] >= r-1:
+            mid[1] = n
+        ret = mid[1]-mid[0]+1
+        if l< mid[0]:
+            ret += l
+        if r > mid[1]:
+            ret += n-r+1
+        print(ret,end=" ")
+        # print(mid,l,r)
+    print()
+        
+for i in range(int(input())):
+    solve()
---
+++ CF2068J.py
@@ -0,0 +1,27 @@
+﻿def solve():
+    n = int(input())
+    s = input()
+    lr = s[:n].count('R')
+    rw = s[n:].count('W')
+    if lr != rw:
+        print('NO')
+        return
+    if (n-lr)%2 != 0 or (n-rw)%2!=0:
+        print('NO')
+        return
+    wi = 0
+    while s[wi] == 'W':
+        wi+=1
+    if wi < n-lr-wi:
+        print('NO')
+        return
+    ri = 0
+    while s[2*n-ri-1] == 'R':
+        ri+=1
+    if ri<n-rw-ri:
+        print('NO')
+        return
+    print('YES')
+
+for i in range(int(input())):
+    solve()
