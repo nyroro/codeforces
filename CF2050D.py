5e8770a2beb1dde2699bbc7767a81ccdc15eb9eb
nyroro <359779828@qq.com>
2025-04-14 15:06:50 +0800

    daily

---
+++ CF2050D.py
@@ -0,0 +1,18 @@
+﻿def solve():
+    s = input()
+    arr = [int(t) for t in s]
+    n = len(arr)
+    for i in range(1,n):
+        for j in range(max(0,i-9), i):
+            v = i-j
+            if arr[i]-v > arr[j]:
+                t = arr[i]-v
+                for k in range(i, j, -1):
+                    arr[k] = arr[k-1]
+                arr[j] = t
+                break
+        # print(arr)
+    print(''.join(str(t) for t in arr))
+        
+for i in range(int(input())):
+    solve()
---
+++ CF2050E.py
@@ -0,0 +1,34 @@
+﻿def solve():
+    a = input()
+    b = input()
+    c = input()
+    n = len(a)
+    m = len(b)
+    dp = [[0x7fffffff]*(n+1) for i in range(m+1)]
+    dp[0][0] = 0
+    for i in range(1,n+1):
+        if a[i-1] == c[i-1]:
+            dp[0][i] = dp[0][i-1]
+        else:
+            dp[0][i] = dp[0][i-1]+1
+    for i in range(1,m+1):
+        if b[i-1] == c[i-1]:
+            dp[i][0] = dp[i-1][0]
+        else:
+            dp[i][0] = dp[i-1][0]+1
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            if c[i+j-1] == a[j-1]:
+                dp[i][j] = min(dp[i][j], dp[i][j-1])
+            else:
+                dp[i][j] = min(dp[i][j], dp[i][j-1]+1)
+            if c[i+j-1] == b[i-1]:
+                dp[i][j] = min(dp[i][j], dp[i-1][j])
+            else:
+                dp[i][j] = min(dp[i][j], dp[i-1][j]+1)
+        # print(dp[i])
+    print(dp[m][n])
+            
+
+for i in range(int(input())):
+    solve()
