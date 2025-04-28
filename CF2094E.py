ab4eb8b938b4489f3eef426bc23ad49f8a2c8b11
nyroro <359779828@qq.com>
2025-04-28 10:51:06 +0800

    daily

---
+++ CF2094E.py
@@ -0,0 +1,25 @@
+﻿def solve():
+    n = int(input())
+    a = list(map(int,input().split()))
+    s = set(a)
+    q = list(s)
+    if len(q) == 1:
+        print(0)
+        return
+    arr = [[0,0] for i in range(30)]
+    for t in a:
+        for i in range(30):
+            j = t&1
+            t>>=1
+            arr[i][j] += 1
+    ans = 0
+    for t in a:
+        ret = 0
+        for i in range(30):
+            j = t&1
+            t>>=1
+            ret += arr[i][1-j]*(1<<i)
+        ans = max(ret, ans)
+    print(ans)
+for i in range(int(input())):
+    solve()
---
+++ CF2097B.py
@@ -0,0 +1,91 @@
+﻿def solve():
+    n,m,k = map(int,input().split())
+    a = []
+    for i in range(k+1):
+        x,y = map(int,input().split())
+        a.append((x,y))
+    table = {}
+    edg = []
+    cycle = [False]*(2*k)
+
+    for i in range(k):
+        px,py = a[i]
+        nx,ny = a[i+1]
+        if abs(px-nx) + abs(py-ny) != 2:
+            print(0)
+            return
+        if px==nx:
+            x,y = px, (py+ny)//2
+            if (x,y) not in table:
+                index = len(table)
+                table[(x,y)] = index
+            else:
+                index = table[(x,y)]
+            if not cycle[index]:
+                cycle[index] = True
+            else:
+                print(0)
+                return
+        elif py == ny:
+            x,y = (px+nx)//2, py
+            if (x,y) not in table:
+                index = len(table)
+                table[(x,y)] = index
+            else:
+                index = table[(x,y)]
+                
+            if not cycle[index]:
+                cycle[index] = True
+            else:
+                print(0)
+                return
+        else:
+            x,y = px, ny
+            if (x,y) not in table:
+                i1 = len(table)
+                table[(x,y)] = i1
+            else:
+                i1 = table[(x,y)]
+            x,y = nx,py
+            if (x,y) not in table:
+                i2 = len(table)
+                table[(x,y)] = i2
+            else:
+                i2 = table[(x,y)]
+            edg.append((i1,i2))
+    
+    m = len(table)
+    p = list(range(m))
+    cnt = [0]*m
+    s = [1]*m
+    for i in range(m):
+        if cycle[i]:
+            cnt[i] = 1
+    for x,y in edg:
+        while p[x]!=x:
+            x = p[x]
+        while p[y]!=y:
+            y = p[y]
+        if x!=y:
+            p[y] = x
+            s[x] += s[y]
+            cnt[x] = cnt[x]+cnt[y]+1
+            cycle[x] |= cycle[y]
+        else:
+            cnt[x]+=1
+    ret = 1
+    mod = int(1e9)+7
+    for i,t in enumerate(cnt):
+        if p[i] == i:
+            if s[i] <cnt[i]:
+                print(0)
+                return
+            elif s[i]==cnt[i]:
+                if not cycle[i]:
+                    ret = (ret*2)%mod
+            else:
+                ret = (ret*s[i])%mod
+    print(ret)
+
+for i in range(int(input())):
+    solve()
